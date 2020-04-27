a838ef776d4575713a809e170b81c51a5fceb395
Sean Barton <linglither@gmail.com>
2020-04-27 09:03:03 -0600

    first commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Sean Barton on 2020-04-12.
+//  Copyright © 2020 Sean Barton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
